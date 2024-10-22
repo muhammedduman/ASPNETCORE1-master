9214743bc1a38455d66aff635c72b5dba6a12420
Muhammed Duman <41876311+muhammedduman@users.noreply.github.com>
2024-10-22 12:42:45 +0300

    ASPNETCOREDers1-master
    ASPNETCOREDers1

---
+++ ASPNETCOREDers1-master/Ders1/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using Ders1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.InteropServices;
+
+namespace Ders1.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            var product_ = new List<Product>
+            {
+                new Product { Id = 1,ProductName="Kalem",Category="Kırtasiye",Price=5},
+                new Product { Id = 2,ProductName="Silgi",Category="Kırtasiye",Price=3},
+                new Product { Id = 3,ProductName="Defter",Category="Kırtasiye",Price=7},
+                new Product { Id = 4,ProductName="Cetvel",Category="Kırtasiye",Price=10}
+            };
+
+            //var iller = new List<string> { "Adana", "Düzce" };
+            //var sayilar = new List<int> { 1,81 };
+
+            return View(product_);
+        }
+        public IActionResult Index2()
+        {
+            return View();
+        }
+    }
+}
---
+++ ASPNETCOREDers1-master/Ders1/Models/Book.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ders1.Models
+{
+	public class Book
+	{
+        [Key]
+        public int Id { get; set; }
+        public string BookName { get; set; }
+        public string Writer { get; set; }
+
+        public int Price { get; set; }
+    }
+}
